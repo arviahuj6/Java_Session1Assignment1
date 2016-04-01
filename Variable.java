38ba0eb08aa5e950202aa5eb9158dd4f1d9ff954
arviahuj6 <geetika.arvind@gmail.com>
2016-04-01 13:28:01 +0530

    Added files via upload

---
+++ Variable.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// Variable + Float
+		int x, answer;
+		float y;
+		x=10;
+		y=(float)11.5;		
+		answer = (int) (x + y);
+		System.out.println("Answer=" + answer);
+	}
+
+}
